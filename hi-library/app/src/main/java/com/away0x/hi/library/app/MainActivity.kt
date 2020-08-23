a14021f674e96a3b4918535bab0cfd6f403d3ae0
Away0x <wutong0910@foxmail.com>
2020-08-23 21:21:14 +0800

    hi-library hilog

---
+++ hi-library/app/src/main/java/com/away0x/hi/library/app/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.away0x.hi.library.app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.away0x.hi.library.app.demo.HiLogDemoActivity
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.tv_log -> {
+                startActivity(Intent(this, HiLogDemoActivity::class.java))
+            }
+        }
+    }
+}
